fdd47e3e45a7cfbe774a5666c57c67c06219b7a5
Corbin HOPPER <choppe1@ds2.CS.McGill.CA>
2018-04-26 14:38:54 -0400

    minor change to run.sh

--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@ if [ "$#" -ne 2 ]; then
     echo "Illegal number of parameters. Need #workers, then input_file."
 fi
 
-nohup mpirun --mca mpi_warn_on_fork 0 -n $1 python3 /home/2014/choppe1/Documents/evoEvo/src/simulators/evolve_root.py /home/2014/choppe1/Documents/evoEvo/data/input/$2 > /home/2014/choppe1/Documents/evoEvo/data/output/serverout &
+nohup mpirun --mca mpi_warn_on_fork 0 -n $1 python3 /home/2014/choppe1/Documents/evoEvo/src/simulators/evolve_root.py /home/2014/choppe1/Documents/evoEvo/data/input/$2 > /home/2014/choppe1/Documents/evoEvo/data/output/clog &

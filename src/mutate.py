c540071996381c0e88a126203ea1b1808d061a0f
Corbin HOPPER <choppe1@ds2.CS.McGill.CA>
2018-07-06 14:00:59 -0400

    .:wq.

--- src/mutate.py
+++ src/mutate.py
@@ -57,8 +57,8 @@ def add_nodes(net, num_add, configs, biases=None, layer = None):
             if not layer: net.node[new_node]['layer'] = 'hidden'
             else:
                 net.node[new_node]['layer'] = layer
-                if layer=='input': net['input_nodes'].append(new_node)
-                elif layer=='output': net['output_nodes'].append(new_node)
+                if layer=='input': net.graph['input_nodes'].append(new_node)
+                elif layer=='output': net{'output_nodes'}.append(new_node)
 
         # ADD EDGE TO NEW NODE TO KEEP CONNECTED
         if biases and bias_on=='edges': add_this_edge(net, configs, node1=new_node, given_bias=biases[i])

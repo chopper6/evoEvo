7797036bc65cd432762c9719fbe75565c3a9e347
chopper6 <corbin.hopper@mail.mcgill.ca>
2018-07-09 19:40:08 -0400

    fixing backprop

--- src/reservoir.py
+++ src/reservoir.py
@@ -25,20 +25,16 @@ def activation(net, node, configs):
     sum = 0
     num_active = 0
     for edge in net.in_edges(node):
-        #use previous state, since state is reserved for the new iteration
+        #use previous state, since state is reserved for the new iteration (see step_fwd())
         if net.node[edge[0]]['prev_state'] != None:
             edge_val = net.node[edge[0]]['prev_state'] * net[edge[0]][edge[1]]['weight']
 
-            # ensure edge bounds, poss change later
-            net[edge[0]][edge[1]]['weight'] = min(net[edge[0]][edge[1]]['weight'], 1)
-            net[edge[0]][edge[1]]['weight'] = max(net[edge[0]][edge[1]]['weight'], -1) 
-            #assert(edge_val >= -1 and edge_val <= 1)     # assumes weights in [-1,1]
             sum += edge_val
             num_active += 1
         sum += net.node[node]['neuron_bias']
 
     #if num_active > 0: sum /= num_active #don't normalize like this, since will always be < 1
-    if num_active == 0: return None
+    # if num_active == 0: return None
     # does bias should take care of this?????????
 
     if activation_fn == 'linear':
@@ -147,19 +143,21 @@ def stochastic_backprop(net, configs, ideal_output):
             print('\nbackprop(): output of node = ' + str(output) + ", with err " + str(err))
 
             # assumes sigmoid
-            delta = (ideal_output[i]-output)*output*(1-output)
+            delta = (output-ideal_output[i])*output*(1-output) #the partial derivative of MSE by the input given to the output neuron
             for in_edge in net.in_edges(output_node):
                 if net.node[in_edge[0]]['state']:
-                    weight_contribution = net.node[in_edge[0]]['state'] #*net[in_edge[0]][in_edge[1]]['weight']
+                    weight_contribution = net.node[in_edge[0]]['state']
+                    assert(net.node[in_edge[0]] is not net.node[output_node]) #TODO: rm this when output out loops are allowed
+
                     partial_err = delta * weight_contribution
                     print("delta = " + str(delta) + ", weight_contrib = " + str(weight_contribution) + ", curr_weight = " + str(net[in_edge[0]][in_edge[1]]['weight']))
-                    net[in_edge[0]][in_edge[1]]['weight'] += partial_err*learning_rate
+                    net[in_edge[0]][in_edge[1]]['weight'] -= partial_err*learning_rate
                     print("now weight = " + str(net[in_edge[0]][in_edge[1]]['weight']))
 
             # calc for bias
-            partial_err = delta * net.node[output_node]['neuron_bias']
-            print("old bias = " + str(net.node[output_node]['neuron_bias']) + ", its err contrib = " + str(partial_err))
-            net.node[output_node]['neuron_bias'] += partial_err*learning_rate
+            # could add diff learning rate for the bias (typically lower)
+            print("old bias = " + str(net.node[output_node]['neuron_bias']) + ", its err contrib is delta = " + str(delta))
+            net.node[output_node]['neuron_bias'] -= delta*learning_rate
             print("new bias = " + str(net.node[output_node]['neuron_bias']))
 
         else: num_active_outputs -= 1
